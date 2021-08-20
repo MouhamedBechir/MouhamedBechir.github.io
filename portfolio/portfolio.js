3eac3ae265d04ee3255d5bce1c3ba80280e5e451
MouhamedBechir <86977748+MouhamedBechir@users.noreply.github.com>
2021-08-20 18:17:55 +0100

    Update portfolio.js

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -48,22 +48,4 @@ $(document).ready(function(){
             }
         }
     });
-    $('.carousel2').owlCarousel({
-        margin: 20,
-        loop: false,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
 });
